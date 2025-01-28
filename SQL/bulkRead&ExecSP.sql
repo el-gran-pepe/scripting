df4aa9002ea132eecd9c455c593fb4b5f1494549
el-gran-pepe <135125063+el-gran-pepe@users.noreply.github.com>
2025-01-28 13:29:49 -0300

    Create bulkRead&ExecSP.sql

---
+++ SQL/bulkRead&ExecSP.sql
@@ -0,0 +1,35 @@
+USE [NombreDeLaBaseDeDatos];
+
+-- Lista de usuarios a quien otorgamos permisos
+DECLARE @Usuarios TABLE (Nombre NVARCHAR(100));
+INSERT INTO @Usuarios (Nombre)
+VALUES
+    ('Usuario1'),
+    ('Usuario2'),
+    ('Usuario3'),
+    ('Usuario4');
+
+-- Otorgar permisos a cada usuario
+DECLARE @Usuario NVARCHAR(100);
+DECLARE @SQL NVARCHAR(MAX);
+
+-- Conceder VIEW DEFINITION y EXECUTE
+DECLARE CursorUsuarios CURSOR FOR
+SELECT Nombre FROM @Usuarios;
+
+OPEN CursorUsuarios;
+FETCH NEXT FROM CursorUsuarios INTO @Usuario;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @SQL = 'GRANT VIEW DEFINITION ON DATABASE::[NombreDeLaBaseDeDatos] TO [' + @Usuario + '];';
+    EXEC sp_executesql @SQL;
+
+    SET @SQL = 'GRANT EXECUTE ON SCHEMA::[dbo] TO [' + @Usuario + '];';
+    EXEC sp_executesql @SQL;
+
+    FETCH NEXT FROM CursorUsuarios INTO @Usuario;
+END;
+
+CLOSE CursorUsuarios;
+DEALLOCATE CursorUsuarios;
