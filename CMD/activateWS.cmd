b140db056cec85035d0949e57f48aad3596f9a87
el-gran-pepe <135125063+el-gran-pepe@users.noreply.github.com>
2025-06-11 09:42:18 -0300

    Create activateWS.cmd

---
+++ CMD/activateWS.cmd
@@ -0,0 +1,3 @@
+REM se debera modificar el argumento "Set-Edition" por la edicion correspondiente al caso asi como el "ProductKey"
+  
+DISM /online /Set-Edition:ServerStandard /ProductKey:XXXXX-XXXXX-XXXXX-XXXXX-XXXXX /AcceptEula
