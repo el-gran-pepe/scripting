77824468114306d435a90499b21dee002c160115
el-gran-pepe <135125063+el-gran-pepe@users.noreply.github.com>
2025-06-11 09:50:58 -0300

    Update deleteFiles.ps1

--- Powershell/deleteFiles.ps1
+++ Powershell/deleteFiles.ps1
@@ -4,13 +4,13 @@ $carpetaRaiz = "Ruta"
 # Obtenemos la fecha actual
 $fechaActual = Get-Date
 
-# Definir la antigüedad en días
+# Definimos la antigüedad en días
 $antiguedad = 30
 
 # Obtiene archivos en todas las subcarpetas que superen la antigüedad
 $archivosAntiguos = Get-ChildItem -Path $carpetaRaiz -Recurse | Where-Object { $_.LastWriteTime -lt ($fechaActual.AddDays(-$antiguedad)) }
 
-# Eliminar archivos antiguos
+# Elimina archivos antiguos
 foreach ($archivo in $archivosAntiguos) {
     Write-Host "Eliminando archivo: $($archivo.FullName)"
     Remove-Item $archivo.FullName -Force
