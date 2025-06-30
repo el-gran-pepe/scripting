1daaf61d85282efaf7ea79fad8ca45b2c904bd35
el-gran-pepe <135125063+el-gran-pepe@users.noreply.github.com>
2025-06-30 15:09:42 -0300

    Update activateWS.cmd

--- CMD/activateWS.cmd
+++ CMD/activateWS.cmd
@@ -1,3 +1,15 @@
 REM se debera modificar el argumento "Set-Edition" por la edicion correspondiente al caso asi como el "ProductKey"
   
 DISM /online /Set-Edition:ServerStandard /ProductKey:XXXXX-XXXXX-XXXXX-XXXXX-XXXXX /AcceptEula
+
+REM si la version ya estaria en "ServerStandard", tendriamos que ejecutar lo siguiente
+
+slmgr /ipk <ProductKey>
+
+REM como podemos ver en que version estamos?
+
+DISM /Online /Get-CurrentEdition
+
+REM o tambien
+
+slmgr.vbs /xpr
