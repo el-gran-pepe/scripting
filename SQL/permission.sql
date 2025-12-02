20dd0c0f56921e5dd63a4e4101ce0e5fe80a7082
el-gran-pepe <135125063+el-gran-pepe@users.noreply.github.com>
2025-12-02 16:42:33 -0300

    Update permission.sql

--- SQL/permission.sql
+++ SQL/permission.sql
@@ -2,3 +2,7 @@
 USE 'Database'
 GRANT VIEW DEFINITION TO [User]
 
+#otorgar permisos de SQLAgent para ver los Jobs
+USE msdb;
+EXEC sp_addrolemember 'SQLAgentReaderRole', 'Usuario'
+#roles: SQLAgentUserRole (solo ve los jobs que genero el propio usuario)| SQLAgentReaderRole (ve todos los ) | SQLAgentOperatorRole
